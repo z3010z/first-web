3d4d14289cf501628a8c92485c719fe0543918e9
z3010z <zivlee094@gmail.com>
2023-02-10 12:12:16 +0800

    20230210

--- 2-10.py
+++ 2-10.py
@@ -1,16 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
-#print(r.text)
+r = requests.get("https://pypi.org/project/beautifulsoup4/")
+# print(r.text)
 
 soup = BeautifulSoup(r.text,"html.parser")
 
-#print(soup)
-sel = soup.select("div.title a")
+# print(soup)
+sel = soup.select("div.split-layout p")
 #dic 中抓 title 裡面span的tag (沒span就是把title裡面全部抓下來)
-print(sel)
+#print(sel)
 
 for s in sel:
-    print(s["href"],"  -  ", s.text)
+    print(s["class"],"  -  ", s.text)
     print("~~~~")
