0e2cd61dd7701a25d782fff2518608518bcce934
z3010z <zivlee094@gmail.com>
2023-02-13 10:44:30 +0800

    2-12.py

--- web.py
+++ web.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.ptt.cc/man/MobileComm/D55F/D271/index.html")
-# print(r.text)
+r = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
+#print(r.text)
 
 soup = BeautifulSoup(r.text,"html.parser")
 
-# print(soup)
-sel = soup.select("div.m-ent")
+#print(soup)
+sel = soup.select("div.title a")
 #dic 中抓 title 裡面span的tag (沒span就是把title裡面全部抓下來)
 print(sel)
 
 for s in sel:
-    print(s.text)
+    print(s["href"],"  -  ", s.text)
     print("~~~~")
-    # ,"  -  ", s.text
