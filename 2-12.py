186d7c3591a900b5b20d44a21765116c8fda6cc9
z3010z <zivlee094@gmail.com>
2023-02-13 09:33:16 +0800

    test

---
+++ 2-12.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://ca.nycu.edu.tw/installation/")
+# print(r.text)
+
+soup = BeautifulSoup(r.text,"html.parser")
+
+# print(soup)
+sel = soup.select("div.title a")
+#dic 中抓 title 裡面span的tag (沒span就是把title裡面全部抓下來)
+#print(sel)
+
+for s in sel:
+    print(s["href"],"  -  ", s.text)
+    print("~~~~")
